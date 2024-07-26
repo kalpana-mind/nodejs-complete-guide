4a949d3c15edb2ded64a770a3607a0adc06bfa7b
kalpana-mind <152434265+kalpana-mind@users.noreply.github.com>
2024-07-26 11:00:20 -0400

    Final graphql changes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const { graphqlHTTP } = require('express-graphql');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 // const feedRoutes = require('./routes/feed');
 // const authRoutes = require('./routes/auth');
@@ -49,14 +51,35 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE'
+    'OPTIONS, GET, POST, PATCH, PUT, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
 // app.use('/feed', feedRoutes);
 // app.use('/auth', authRoutes);
+
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res.status(201).json({
+    message: 'File stored.',
+    filePath: req.file.path.replace('\\', '/'),
+  });
+});
 
 app.use(
   '/graphql',
@@ -64,6 +87,15 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error occurred.';
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    },
   })
 );
 
